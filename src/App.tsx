8d0aee31c8754fe3bb8d30e77a47a63987a198aa
bharathmk66-cmd <bharath.mk66@gmail.com>
2025-11-02 09:54:04 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SymptomChecker from './symptom_checker_ui';
+
+function App() {
+  return (
+    <div className="App">
+      <SymptomChecker />
+    </div>
+  );
+}
+
+export default App;
